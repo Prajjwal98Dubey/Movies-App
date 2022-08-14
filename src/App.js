6f0aee07dd0761d606ed72d4e764ba6159c42244
Prajjwal98Dubey <110735413+Prajjwal98Dubey@users.noreply.github.com>
2022-08-14 21:50:36 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import MovieCard from './components/MovieCard';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const[movies,setMovies]=useState([]);
+  const[term,setTerm]=useState('')
+  const[isloading,setisLoading]=useState(true)
+
+  const displayMovie=(title)=>{
+    axios.get(`https://omdbapi.com/?s=${title}&apikey=f5f53ff4`)
+    .then(res=>{
+      setMovies(res.data.Search)
+       setisLoading(false)})
+  }
+  useEffect(()=>{
+    displayMovie("batman")
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <div className='search-box'>
+      <input type="text" value={term} onChange={(e)=>setTerm(e.target.value)}/>
+      <button className='search-btn' onClick={()=>displayMovie(term)}>Search</button>
+     </div>
+     {/* { isloading === false && movies.length===0 && (<h1 className='no-movie'>No Movie Found for this search</h1>)} */}
+     {isloading ? (<h1 className='load-tag'>Loading...</h1>):
+     <div className='container'>
+      <div className='grid'>
+      {movies.map(movie=>(
+       <MovieCard key={movie.imdbID} movie={movie}/>
+      ))}
+      </div>
+     </div>}
+  
+     </>
   );
 }
 
